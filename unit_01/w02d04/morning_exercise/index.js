3d10dbbc524a30143919f855f16353e7de12a99f
FHappy <Hassan.Masroor@me.com>
2017-03-02 11:03:28 -0500

    morning exercise 3/2/16

--- unit_01/w02d04/morning_exercise/index.js
+++ unit_01/w02d04/morning_exercise/index.js
@@ -4,9 +4,38 @@
 "use strict";
 /// DO NOT EDIT ABOVE THIS LINE ///
 
-var makeChange = function(price, payment) {
-  // Your Code Here
-};
+function makeChange(price, payment) {
+  if (payment < price) {
+    return "Not enough money you broke ass trick";
+  }
+
+  var change = (payment - price) * 100;
+  function coinAmount (currChange, coinValue) {
+    var tempCoin = (currChange / coinValue);
+    var numOfCoins = Math.floor(tempCoin);
+    change = Math.floor((tempCoin - numOfCoins) * coinValue);
+    return numOfCoins;
+  }
+
+  var quarters = coinAmount(change, 25);
+  var dimes = coinAmount(change, 10);
+  var nickels = coinAmount(change, 5);
+  var pennies = change + 1;
+  var result = [quarters, dimes, nickels, pennies];
+
+  return result;
+  //ALTERNATIVELY
+  // var tempQuarters = (change / 25);
+  // var quarters = Math.floor(tempQuarters);
+  // change = Math.floor((tempQuarters - quarters) * 25);
+  // var tempDimes = (change / 10);
+  // var dimes = Math.floor(tempDimes);
+  // change = Math.floor((tempDimes - dimes) * 10);
+  // var tempNickels = (change / 5);
+  // var nickels = Math.floor(tempNickels);
+  // change = Math.floor((tempNickels - nickels) * 5);
+
+}
 
 /// DO NOT EDIT BELOW THIS LINE ///
 module.exports = {
