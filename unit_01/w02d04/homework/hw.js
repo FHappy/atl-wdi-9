9d362f8602d298bffbd9114b2fdc4e35e2a55550
FHappy <Hassan.Masroor@me.com>
2017-03-02 23:50:07 -0500

    homework and labs 3/2/16

--- unit_01/w02d04/homework/hw.js
+++ unit_01/w02d04/homework/hw.js
@@ -16,6 +16,7 @@ const CounterCollection = {
     });
     return this.lastCountId;
   },
+
   getCounterValue: function(countId){
     console.log(`read counter #${countId}`);
     let counter = this.counters.find(function(counter){
@@ -23,6 +24,7 @@ const CounterCollection = {
     });
     if (counter) { return counter.count; }
   },
+
   incrementCounter: function(countId){
     console.log(`increment counter #${countId}`);
     let counter = this.counters.find(function(counter){
@@ -30,18 +32,23 @@ const CounterCollection = {
     });
     if (counter) {
       counter.count += 1;
+      console.log('count is ' + counter.count);
       return counter.count;
     }
   },
+
   destroyCounter: function(countId){
     console.log(`destroy counter #${countId}`);
     let counter = this.counters.find(function(counter){
       return counter.countId === countId;
     });
-    if (counter) { counter.destroy(); }
-    this.counters = this.counters.filter(function(counter){ //
-      return counter.countId !== countId
-    });
+    if (counter) {
+      console.log(counter);
+      // counter.destroy(); }
+      this.counters = this.counters.filter(function(counter){ //
+        return counter.countId !== countId
+      });
+    }
   }
 };
 
@@ -49,15 +56,45 @@ const CounterCollection = {
 const Presenter = {
   insertCounterComponent: function(newCountId){
     console.log(`insert counter component #${newCountId}`);
-    // Your Code Here
+    var $counterContainer = $('#counter-list');
+    var $counterDiv = $('<div>').addClass('counter')
+                                .attr('data-index', '2')
+                                .attr('data-countId', newCountId);
+    $counterContainer.append($counterDiv);
+    var $counterH3 = $('<h3>').html('Count: <span id="counter' +
+                                    newCountId +
+                                    '">0</span>');
+    $counterDiv.append($counterH3);
+    var $incrementButton = $('<button>')
+                            .addClass('increment')
+                            .html(' + 1');
+    var $deleteButton = $('<button>')
+                        .addClass('delete')
+                        .html("Delete");
+    $counterDiv.append($incrementButton);
+    $counterDiv.append($deleteButton);
+    $incrementButton.click(AppController.onClickIncrement);
+    $deleteButton.click(AppController.onClickDelete);
   },
   refreshCounterComponent: function(countId){
     console.log(`refresh counter component #${countId}`);
     // Your Code Here
+    var incrementValue = CounterCollection.getCounterValue(countId);
+    var spanIdStr = "#counter" + countId;
+    var $spanElem = $(spanIdStr);
+    $spanElem.html(incrementValue);
   },
   removeCounterComponent: function(countId){             // REACH
     console.log(`remove counter component #${countId}`);
     // Your Code Here
+    var $counterDivList = $(".counter");
+    var divToDel;
+    for (var i = 0; i < $('.counter').length; i++) {
+      if (parseInt($('.counter')[i].getAttribute('data-countId')) === countId) {
+        divToDel = $('.counter')[i];
+      }
+    }
+    divToDel.remove();
   }
 };
 
@@ -65,14 +102,29 @@ const Presenter = {
 const AppController = {
   onClickNewCounter: function(event){
     // Your Code Here
+    console.log(this);
+    CounterCollection.createCounter();
+    var newCountId = CounterCollection.lastCountId;
+    Presenter.insertCounterComponent(newCountId);
   },
+
   onClickIncrement: function(event){
     // Your Code Here
+    var $counterDiv = $(this).parent();
+    var divCountId = parseInt($counterDiv.attr('data-countId'));
+    CounterCollection.incrementCounter(divCountId);
+    Presenter.refreshCounterComponent(divCountId);
   },
+
   onClickDelete: function(event){                           // REACH
     // Your Code Here
+    var $counterDiv = $(this).parent();
+    var divCountId = parseInt($counterDiv.attr('data-countId'));
+    CounterCollection.destroyCounter(divCountId);
+    Presenter.removeCounterComponent(divCountId);
   }
 };
+
 
 window.onload = function(){
   document.getElementById('new-counter').onclick = AppController.onClickNewCounter;
---
+++ unit_01/w02d04/student_labs/homes/script.js
@@ -0,0 +1,55 @@
+console.log('hey');
+
+$(document).ready(function() {
+  // alert("Everything is ready, let's do this.");
+
+  $('#addHome').removeClass('btn-danger').addClass('btn-success');
+  // $('#addHome').toggleClass('btn-danger' 'btn-success');
+
+  // $('.jumbotron').addClass('text-center');
+
+  var newLink = $('<br><br><a id="zillowLink" href="http://www.zillow.com">Visit Zillow.com</a>');
+  newLink.appendTo('body');
+
+  $('#zillowlink').attr("target", "_blank");
+
+  var index = 0;
+
+  $('#addHome').on('click', function(){
+    if (index < newHomes.length) {
+      var home = newHomes[index];
+      var newRow = $('<tr>');
+      $('tbody').append(newRow);
+
+    for (key in home) {
+      var cell = $('<td>');
+      var cellData = home[key];
+      cell.html(cellData);
+      newRow.append(cell);
+    }
+
+    newRow.append('<td><button class="btn btn-xs btn-danger">Remove</button></td>');
+    index++;
+    }
+  });
+
+  $('.btn-danger').on('click', function($event) {
+    alert('you are about ot delete this row.');
+    console.log($(this));
+    var $rowThatWasClicked = $(this).parent().parent();
+    $rowThatWasClicked.fadeOut(1000, function() {
+        $rowThatWasClicked.remove();
+    });;
+  });
+
+
+  var newHomes = [
+    {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
+    {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
+    {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
+    {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
+];
+});
+
+
+
