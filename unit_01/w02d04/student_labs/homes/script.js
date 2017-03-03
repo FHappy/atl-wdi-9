f3e957ea9301d981df2d7d10d7dfb89bda7d4867
FHappy <Hassan.Masroor@me.com>
2017-03-03 00:26:55 -0500

    3/2/16

--- unit_01/w02d04/student_labs/homes/script.js
+++ unit_01/w02d04/student_labs/homes/script.js
@@ -21,15 +21,19 @@ $(document).ready(function() {
       var newRow = $('<tr>');
       $('tbody').append(newRow);
 
-    for (key in home) {
-      var cell = $('<td>');
-      var cellData = home[key];
-      cell.html(cellData);
-      newRow.append(cell);
-    }
+      for (key in home) {
+        var cell = $('<td>');
+        var cellData = home[key];
+        cell.html(cellData);
+        newRow.append(cell);
+      }
 
-    newRow.append('<td><button class="btn btn-xs btn-danger">Remove</button></td>');
-    index++;
+      newRow.append(
+        '<td><button class="btn btn-xs ' +
+        'btn-danger">Remove</button></td>'
+        );
+
+      index++;
     }
   });
 
@@ -48,7 +52,7 @@ $(document).ready(function() {
     {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
     {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
     {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
-];
+  ];
 });
 
 
