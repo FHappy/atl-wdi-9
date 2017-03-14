6d011a6484595d080325329de58a4d18c48a132e
FHappy <Hassan.Masroor@me.com>
2017-03-14 17:33:50 -0400

    completed GET routes

--- unit_02/w06d02/homework/pizza-express/app.js
+++ unit_02/w06d02/homework/pizza-express/app.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+
+app.get('/', function(req, res, next) {
+  res.send('Welcome to Pizza Express!');
+})
+
+app.get('/topping/:type', function(req, res, next) {
+  res.send(`${req.params.type} pizza! Good choice.`);
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+  res.send(`Your order for ${req.params.amount} ${req.params.size}` +
+           ` pizzas will be ready in 1 minute!`);
+});
+
 app.listen(port, function() {
   console.log("==========================");
   console.log('LISTENING ON PORT ' + port);
