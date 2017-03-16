6cc72e2379e5b133a2db4e0f5a4ac81593592d91
FHappy <Hassan.Masroor@me.com>
2017-03-16 14:10:19 -0400

    minor syntax adjustments

--- unit_02/w06d04/student_labs/recipes_app/server.js
+++ unit_02/w06d04/student_labs/recipes_app/server.js
@@ -2,56 +2,20 @@ var express = require('express');
 var app = express();
 var hbs = require('hbs');
 
-var recipes = require('./recipes.js');
-app.set('view engine', 'hbs');
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var recipesController = require('./controllers/recipes_controller.js');
-app.use('/recipes', recipesController);
-
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/recipes', function(req, res) {
-//   res.send('route created, skkkk');
-// });
+var recipes = require('./recipes.js');
+app.set('view engine', 'hbs');
 
-// app.get('/recipes', function(req,res) {
-//   res.render('index.hbs', {
-//     recipes: recipes.seededRecipes
-//   });
-// });
-//
-// app.get('/recipes/new', function(req, res) {
-//     res.render('new.hbs');
-// });
-//
-// app.post('/recipes/', function(req, res) {
-//   var newVeggie = true;
-//   if (req.body.veggie === 'false') {
-//     newVeggie = false;
-//   }
-//   var newRecipe = {
-//     title: req.body.title,
-//     veggie: newVeggie,
-//     imageUrl: req.body.imageUrl
-//   }
-//
-//   recipes.seededRecipes.push(newRecipe);
-//   res.redirect('/recipes');
-// });
-//
-// app.get('/recipes/:id', function(req, res) {
-//   var recipe = recipes.seededRecipes[req.params.id];
-//
-//   res.render('show.hbs', {
-//     recipe: recipe
-//   })
-// });
 
+
+var recipesController = require('./controllers/recipes_controller.js');
+app.use('/recipes', recipesController);
 
 
 var port = process.env.PORT || 3000;
