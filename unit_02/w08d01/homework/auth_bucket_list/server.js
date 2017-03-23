9c81e8a57a8f7195bfb7e492630a5537d4d4ea47
FHappy <Hassan.Masroor@me.com>
2017-03-23 17:18:59 -0400

    updated user schema

--- unit_02/w08d01/homework/auth_bucket_list/server.js
+++ unit_02/w08d01/homework/auth_bucket_list/server.js
@@ -1,12 +1,12 @@
 pry = require('pryjs')
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var methodOverride = require('method-override');
-var logger = require('morgan');
-var hbs = require('hbs');
-var mongoose = require('mongoose');
+var express               = require('express');
+var bodyParser            = require('body-parser');
+var session               = require('express-session');
+var methodOverride        = require('method-override');
+var logger                = require('morgan');
+var hbs                   = require('hbs');
+var mongoose              = require('mongoose');
 
 
 
